8d2e608dedf529e42518d0b4900de61b7249935a
dev14-plugthink <pauloabreucoder@gmail.com>
2021-10-31 14:44:28 -0300

    Abstract Factory

--- 02 - Creational - Abstract Factory/Problem/Factories/ICustomerVehicleFactory.ts
+++
@@ -1,7 +0,0 @@
-import { ICustomer } from "../Interfaces/ICustomer";
-import { IVehicle } from "../Interfaces/IVehicle";
-
-export interface CustomerVehicleFactory{
-    createCustomer(customerName: string): ICustomer;
-    createVehicle(vehicleName: string, customerName): IVehicle;
-}
